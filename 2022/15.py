31a45352aa756fff6a3d55ae6fc92dc49c5d284a
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-09-23 19:05:57 +0200

    move into folder

---
+++ 2022/15.py
@@ -0,0 +1,21 @@
+filename = "test.txt"
+
+
+def getcoords(xystr):
+    x, y = xystr.split(", ")
+    return int(x[2:]), int(y[2:])
+
+
+with open(filename) as f:
+    input = f.readlines()
+    input = [l.split("at ")[1:] for l in input]
+    input = [(getcoords(s[0].split(":")[0]), getcoords(s[1].strip())) for s in input]
+
+
+###
+
+sensors, beacons = zip(*input)
+###
+sensors, beacons
+###
+beacons[0]
