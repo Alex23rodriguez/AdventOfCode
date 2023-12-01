2e48e07753c805f0461658eaad95498c547cc06e
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2023-12-01 12:26:45 +0100

    updated template

--- template.py
+++ template.py
@@ -1,35 +1,43 @@
-# imports
-import re
-import json
-from itertools import (
-    combinations,
-    permutations,
-    zip_longest,
-    accumulate,
-    combinations_with_replacement,
-)
-from collections import defaultdict, Counter
-from functools import reduce, lru_cache, partial
+### imports
+# import re
+# import json
+# from itertools import (
+#     combinations,
+#     permutations,
+#     zip_longest,
+#     accumulate,
+#     combinations_with_replacement,
+# )
+# from collections import defaultdict, Counter
+# from functools import reduce, lru_cache, partial
+#
+# import sys
+# sys.path.append('../..')
+# import util
 
-###
+### read files
 test_txt = open("test.txt").read()
-
+test_lines = test_txt.splitlines()
+print(f"read {len((test_lines))} test lines")
 input_txt = open("input.txt").read()
-print(f"read {len(input_txt.splitlines())} lines")
-###
-# read util functions
+input_lines = input_txt.splitlines()
+print(f"read {len((input_lines))} lines")
 
 
-###
-def prepare(inp):
-    lines = inp.splitlines()
-    # UPDATE
-    out = [ln for ln in lines]
+### read functions
+def parse_lines(lines: list[str]):
+    out = [line_func(line) for line in lines]
     return out
 
 
-teststart = prepare(test_txt)
-start = prepare(input_txt)
+###
+def line_func(line: str):
+    # TODO
+    return line
+
+
+teststart = parse_lines(test_lines)
+start = parse_lines(input_lines)
 ###
 # util functions
 
