f88ef3d1eed4358517c09f3a74db0bd26fb3eb83
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2023-12-02 06:27:06 +0100

    2023/02 sol

---
+++ 2023/02/main.py
@@ -0,0 +1,144 @@
+### imports
+import sys
+
+sys.path.append("../..")
+from util import timed
+
+from pathlib import Path
+
+import re
+import json
+from itertools import (
+    combinations,
+    permutations,
+    zip_longest,
+    accumulate,
+    combinations_with_replacement,
+)
+from collections import defaultdict, Counter
+from functools import reduce, lru_cache, partial
+from operator import mul
+
+from util import iden_cross
+from algs import dijkstra, floyd_warshall
+
+### read files
+test_txt = Path("test.txt").read_text()
+test_lines = test_txt.splitlines()
+print(f"read {len((test_lines))} test lines")
+input_txt = Path("input.txt").read_text()
+input_lines = input_txt.splitlines()
+print(f"read {len((input_lines))} lines")
+
+
+### read functions
+def parse_all_lines(lines: list[str], func):
+    out = [func(line) for line in lines]
+    return out
+
+
+### util defenitions
+colors = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
+
+
+### parse input
+def parse_line(line: str):
+    id, txt = line.split(": ")
+    id = int(id.split()[1])
+    sets = txt.split("; ")
+    for s in sets:
+        for c in s.split(", "):
+            n, col = c.split()
+            if colors[col] < int(n):
+                return id, False
+
+    return id, True
+
+
+with timed():
+    teststart = parse_all_lines(test_lines, parse_line)
+    start = parse_all_lines(input_lines, parse_line)
+
+### main
+sum(id for id, poss in teststart if poss)
+###
+
+sum(id for id, poss in start if poss)
+
+###
+# PART 2
+###
+p = Path("test2.txt")
+if p.exists():
+    test_txt = p.read_text()
+    test_lines = test_txt.splitlines()
+
+
+### util defenitions
+
+
+### parse input - cange parse_line if necessary
+# change parse_line if necessary
+def parse_line_2(line: str):
+    id, txt = line.split(": ")
+    id = int(id.split()[1])
+    sets = txt.split("; ")
+    clrs = []
+    for s in sets:
+        d = {}
+        for c in s.split(", "):
+            n, col = c.split()
+            d[col] = int(n)
+        clrs.append(d)
+
+    return clrs
+
+
+with timed():
+    teststart = parse_all_lines(test_lines, parse_line_2)
+    start = parse_all_lines(input_lines, parse_line_2)
+
+### main
+mins = []
+
+for clrs in teststart:
+    m = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    for d in clrs:
+        for k, v in d.items():
+            m[k] = max(m[k], v)
+    mins.append(m)
+
+
+###
+ps = [reduce(mul, m.values()) for m in mins]
+ps
+###
+sum(ps)
+###
+mins = []
+
+for clrs in start:
+    m = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    for d in clrs:
+        for k, v in d.items():
+            m[k] = max(m[k], v)
+    mins.append(m)
+
+
+###
+ps = [reduce(mul, m.values()) for m in mins]
+ps
+###
+sum([reduce(mul, m.values()) for m in mins])
