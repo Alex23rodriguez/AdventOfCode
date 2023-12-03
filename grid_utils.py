f4579f4cded1b88a6584356e3260151621148584
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2023-12-03 08:54:27 +0100

    added wrap option to get_adjecent

--- grid_utils.py
+++ grid_utils.py
@@ -6,6 +6,8 @@ def get_adjacent(
     coord: tuple[int, int],
     diag=True,
     center=False,
+    hwrap=False,
+    vwrap=False,
     criteria: Callable[[Any], bool] = lambda _: True,
 ) -> Generator[tuple[tuple[int, int], Any], None, None]:
     i, j = coord
@@ -16,10 +18,17 @@ def get_adjacent(
     if diag:
         for i2 in (i - 1, i, i + 1):
             if i2 < 0 or i2 == len_i:
-                continue
+                if not vwrap:
+                    continue
+                else:
+                    i2 = 0 if i2 == len_i else len_i - 1
             for j2 in (j - 1, j, j + 1):
                 if j2 < 0 or j2 == len_j:
-                    continue
+                    if not hwrap:
+                        continue
+                    else:
+                        j2 = 0 if j2 == len_j else len_j - 1
+
                 if not center and i2 == i and j2 == j:
                     continue
                 val = grid[i2][j2]
@@ -30,9 +39,15 @@ def get_adjacent(
             yield (i, j), grid[i][j]
         for i2, j2 in ((i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)):
             if i2 < 0 or i2 == len_i:
-                continue
+                if not vwrap:
+                    continue
+                else:
+                    i2 = 0 if i2 == len_i else len_i - 1
             if j2 < 0 or j2 == len_j:
-                continue
+                if not hwrap:
+                    continue
+                else:
+                    j2 = 0 if j2 == len_j else len_j - 1
 
             val = grid[i2][j2]
             if criteria(val):
