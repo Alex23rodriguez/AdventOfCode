6e4a432d1f88e41d80a044ad3a0cd745fc77376e
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2023-12-03 08:48:11 +0100

    rewrote 2023/03 with new util funcs

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -3,8 +3,9 @@ import sys
 
 sys.path.append("../..")
 from util import timed
-
 from pathlib import Path
+
+from grid_utils import get_adjacent, hgrow, get_from_grid
 
 
 ### read files
@@ -16,92 +17,63 @@ input_lines = input_txt.splitlines()
 print(f"read {len((input_lines))} lines")
 
 
-### read functions
-def parse_all_lines(lines: list[str], func):
-    out = [func(line) for line in lines]
-    return out
-
-
 ### util defenitions
-def get_num(lines: list[str], i, j):
-    jl = j
-    jr = j
-    while jl - 1 >= 0 and lines[i][jl - 1] in "0123456789":
-        jl -= 1
-    while jr + 1 < len(lines[0]) and lines[i][jr + 1] in "0123456789":
-        jr += 1
-    return (i, jl), int(lines[i][jl : jr + 1])
+def isnum(x):
+    return x in "0123456789"
+
+
+def get_num(grid, coord):
+    i, _ = coord
+    (_, a), (_, b) = hgrow(grid, coord, isnum)
+    return (i, a), int(grid[i][a : b + 1])
 
 
 ### parse input
-def get_sym_coords(lines: list[str]):
-    syms = []
-    for i, l in enumerate(lines):
-        for j, c in enumerate(l):
-            if c not in "0123456789" and c != ".":
-                syms.append((i, j))
-    return syms
-
-
 with timed():
-    teststart = get_sym_coords(test_lines)
-    start = get_sym_coords(input_lines)
+    test_symbols = list(get_from_grid(test_lines, lambda x: x not in ".1234567890"))
+    input_symbols = list(get_from_grid(input_lines, lambda x: x not in ".1234567890"))
 
 
 ### main
-def check_sym(lines: list[str], coord, good):
-    i, j = coord
-    for di in range(-1, 2):
-        for dj in range(-1, 2):
-            if i + di < 0 or i + di == len(lines) or j + dj < 0 or j + dj == len(lines[0]):
-                continue
-            c = lines[i + di][j + dj]
-            if c in "0123456789":
-                good.append(get_num(lines, i + di, j + dj))
-    return good
-
-
 ### TEST
-good = []
-for coord in teststart:
-    check_sym(test_lines, coord, good)
+adjnums = []
+for coord, _ in test_symbols:
+    adjnums.extend(get_adjacent(test_lines, coord, criteria=isnum))
 
-good
-###
-sum(x[1] for x in set(good))
+fullnums = set(get_num(test_lines, coord) for coord, _ in adjnums)
+print(sum([n for _, n in fullnums]))
 ### INPUT
-good = []
-for coord in start:
-    check_sym(input_lines, coord, good)
+adjnums = []
+for coord, _ in input_symbols:
+    adjnums.extend(get_adjacent(input_lines, coord, criteria=isnum))
 
-###
-sum(x[1] for x in set(good))
+fullnums = set(get_num(input_lines, coord) for coord, _ in adjnums)
+print(sum([n for _, n in fullnums]))
 
 
 ###
 # PART 2
 ###
 ### TEST
-
 ratios = []
-for coord in teststart:
-    i, j = coord
-    if test_lines[i][j] != "*":
-        continue
-    nums = set(check_sym(test_lines, coord, []))
-    if len(nums) == 2:
-        a, b = list(nums)
-        ratios.append(a[1] * b[1])
-sum(ratios)
-###
+for coord, sym in test_symbols:
+    if sym == "*":
+        adjnums = list(get_adjacent(test_lines, coord, criteria=isnum))
+        fullnums = set(get_num(test_lines, coord) for coord, _ in adjnums)
 
+        if len(fullnums) == 2:
+            a, b = fullnums
+            ratios.append(a[1] * b[1])
+print(sum(ratios))
+
+### INPUT
 ratios = []
-for coord in start:
-    i, j = coord
-    if input_lines[i][j] != "*":
-        continue
-    nums = set(check_sym(input_lines, coord, []))
-    if len(nums) == 2:
-        a, b = list(nums)
-        ratios.append(a[1] * b[1])
-sum(ratios)
+for coord, sym in input_symbols:
+    if sym == "*":
+        adjnums = list(get_adjacent(input_lines, coord, criteria=isnum))
+        fullnums = set(get_num(input_lines, coord) for coord, _ in adjnums)
+
+        if len(fullnums) == 2:
+            a, b = fullnums
+            ratios.append(a[1] * b[1])
+print(sum(ratios))
