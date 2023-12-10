05db3bf22b2bda9f0815ca4e96198daab32a27d3
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2023-12-10 08:16:34 +0100

    2023/10 almost part 2

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -87,6 +87,10 @@ def neg(c):
     return (-c[0], -c[1])
 
 
+###
+###
+# at, came from
+# manually set first directions
 ## TEST
 ts = list(get_from_grid(test_lines, lambda x: x == "S"))[0][0]
 grid = teststart
@@ -97,12 +101,23 @@ ts = list(get_from_grid(input_lines, lambda x: x == "S"))[0][0]
 grid = start
 coords = [(add_coords(ts, up), down), (add_coords(ts, down), up)]
 ###
-# at, came from
+# visited = [(ts, "|")]
+visited = {
+    (up, down): [ts],
+    (left, right): [],
+    (up, right): [],
+    (left, up): [],
+    (left, down): [],
+    (right, down): [],
+}
 ans = 1
 while True:
     (c1, p1), (c2, p2) = coords
     if c1 == c2:
+        visited[tuple(grid[c1[0]][c1[1]])].append(c1)
         break
+    visited[tuple(grid[c1[0]][c1[1]])].append(c1)
+    visited[tuple(grid[c2[0]][c2[1]])].append(c2)
 
     ans += 1
 
@@ -123,32 +138,137 @@ while True:
         coords[1] = (add_coords(c2, mv), neg(mv))
 
     if ans == 10000:
-        print("BAD")
+        print("BREAK")
         break
-print("done!", ans)
+
+print(ans)
+###
 
 
-###
-# PART 2
-###
+##
+## PART 2
+##
 p = Path("test2.txt")
 if p.exists():
     test_txt = p.read_text()
     test_lines = test_txt.splitlines()
 
 
-### util defenitions
+##
+def pprint(grid):
+    for l in grid:
+        print("".join(l))
 
 
-### parse input - cange parse_line if necessary
-# change parse_line if necessary
-def parse_line_2(line: str):
-    # TODO
-    return line
+def get(grid, coord):
+    return grid[coord[0]][coord[1]]
 
 
-with timed():
-    teststart = parse_all_lines(test_lines, parse_line_2)
-    start = parse_all_lines(input_lines, parse_line_2)
+def insert(grid, coord, val):
+    grid[coord[0]][coord[1]] = val
 
-### main
+
+def get_next_dir(grid, coord, prev_dir):
+    d1, d2 = get(grid, coord)
+    # print(f'{(d1,d2)=}\n{coord=}\n{prev_dir=}')
+    if d1 == neg(prev_dir):
+        return d2
+    assert d2 == neg(prev_dir)
+    return d1
+
+
+def get_new_indir(prev_dir, next_dir, prev_indir):
+    if prev_dir == next_dir:
+        return prev_indir
+
+    if clockwise[prev_dir] == next_dir:
+        return clockwise[prev_indir]
+
+    assert counterclockwise[prev_dir] == next_dir
+    return counterclockwise[prev_indir]
+
+
+##
+dirsgrid = start
+
+d = {
+    (up, down): "|",
+    (left, right): "-",
+    (up, right): "L",
+    (left, up): "J",
+    (left, down): "7",
+    (right, down): "F",
+}
+
+clockwise = {
+    left: down,
+    down: right,
+    right: up,
+    up: left,
+}
+
+counterclockwise = {
+    left: up,
+    up: right,
+    right: down,
+    down: left,
+}
+###
+from time import sleep
+
+###
+grid = [["." for _ in range(len(dirsgrid[0]))] for _ in range(len(dirsgrid))]
+for dir, sign in d.items():
+    for i, j in visited[dir]:
+        grid[i][j] = sign
+
+insert(dirsgrid, ts, (up, down))
+
+curr = (7, 19)  # manually start at upper left corner
+startcoord = (7, 20)
+prev_dir = left
+indir = down
+assert get(grid, curr) == "F"
+
+inside = []
+failsafe = 0
+while curr != startcoord:
+    insert(grid, curr, "X")
+
+    if get(grid, add_coords(curr, indir)) == ".":
+        inside.append(curr)
+        insert(grid, add_coords(curr, indir), "I")
+
+    next_dir = get_next_dir(dirsgrid, curr, prev_dir)
+    # redirect indir
+    indir = get_new_indir(prev_dir, next_dir, indir)
+
+    # updete curr and came_from
+    curr = add_coords(curr, next_dir)
+    prev_dir = next_dir
+
+    failsafe += 1
+    if failsafe == 15000:
+        print("BAD")
+        break
+
+print("done!")
+
+pprint(grid)
+##
+
+##
+incoords = [c for c, _ in get_from_grid(grid, lambda x: x == "I")]
+for coord in incoords:
+    for adjc, adjv in get_adjacent(grid, coord, diag=False):
+        if adjv == ".":
+            insert(grid, adjc, "I")
+
+
+pprint(grid)
+print(len(incoords))
+###
+# 257 - too low
+# 270 - too high
+# 260 - too low
+# 265 - correct!
