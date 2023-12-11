abe17d8cbdc6f1576bb0f9b9b012aa98bdd16980
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2023-12-11 06:22:34 +0100

    2023/11 part 1

---
+++ 2023/11/main.py
@@ -0,0 +1,94 @@
+### imports
+import sys
+
+sys.path.append("../..")
+from util import timed
+from pathlib import Path
+
+import re
+import json
+from itertools import (
+    combinations,
+    permutations,
+    zip_longest,
+    accumulate,
+    combinations_with_replacement,
+)
+from collections import defaultdict, Counter
+from functools import reduce, lru_cache, partial, cmp_to_key
+import operator as op
+
+from more_itertools import (
+    chunked,
+    sliced,
+    distribute,
+    split_at,
+    split_into,
+    split_when,
+    bucket,
+    windowed,
+    distinct_permutations,
+    distinct_combinations,
+    locate,
+)
+
+from grid_utils import get_adjacent, hgrow, vgrow, get_from_grid
+from graph_utils import iden_cross
+from algs import dijkstra, floyd_warshall
+
+### read files
+test_txt = Path("test.txt").read_text()
+test_lines = test_txt.splitlines()
+print(f"read {len((test_lines))} test lines")
+input_txt = Path("input.txt").read_text()
+input_lines = input_txt.splitlines()
+print(f"read {len((input_lines))} lines")
+
+
+### read functions
+def parse_all_lines(lines: list[str], func):
+    for i in range(len(lines)):
+        if "#" not in lines[-i - 1]:
+            lines.insert(-i, lines[-i - 1])
+    return out
+
+
+### util defenitions
+
+
+### parse input
+def parse_line(line: str):
+    return line
+
+
+with timed():
+    teststart = parse_all_lines(test_lines, parse_line)
+    start = parse_all_lines(input_lines, parse_line)
+
+### main
+
+
+###
+# PART 2
+###
+p = Path("test2.txt")
+if p.exists():
+    test_txt = p.read_text()
+    test_lines = test_txt.splitlines()
+
+
+### util defenitions
+
+
+### parse input - cange parse_line if necessary
+# change parse_line if necessary
+def parse_line_2(line: str):
+    # TODO
+    return line
+
+
+with timed():
+    teststart = parse_all_lines(test_lines, parse_line_2)
+    start = parse_all_lines(input_lines, parse_line_2)
+
+### main
