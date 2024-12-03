79e9435b60ef9f0bc6bacf665a5eb798b062c352
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2024-12-03 07:15:34 +0100

    updated template

--- template.py
+++ template.py
@@ -1,42 +1,26 @@
-### imports
+# %% imports
 import sys
 
 sys.path.append("../..")
-from util import timed
+import json
+import operator as op
+import re
+from collections import Counter, defaultdict
+from functools import cmp_to_key, lru_cache, partial, reduce
+from itertools import (accumulate, combinations, combinations_with_replacement,
+                       permutations, zip_longest)
 from pathlib import Path
 
-import re
-import json
-from itertools import (
-    combinations,
-    permutations,
-    zip_longest,
-    accumulate,
-    combinations_with_replacement,
-)
-from collections import defaultdict, Counter
-from functools import reduce, lru_cache, partial, cmp_to_key
-import operator as op
+from more_itertools import (bucket, chunked, distinct_combinations,
+                            distinct_permutations, distribute, locate, sliced,
+                            split_at, split_into, split_when, windowed)
 
-from more_itertools import (
-    chunked,
-    sliced,
-    distribute,
-    split_at,
-    split_into,
-    split_when,
-    bucket,
-    windowed,
-    distinct_permutations,
-    distinct_combinations,
-    locate,
-)
-
-from grid_utils import get_adjacent, hgrow, vgrow, get_from_grid
-from graph_utils import iden_cross
 from algs import dijkstra, floyd_warshall
+from graph_utils import iden_cross
+from grid_utils import get_adjacent, get_from_grid, hgrow, vgrow
+from util import timed
 
-### read files
+# %% read files
 test_txt = Path("test.txt").read_text()
 test_lines = test_txt.splitlines()
 print(f"read {len((test_lines))} test lines")
@@ -45,16 +29,16 @@ input_lines = input_txt.splitlines()
 print(f"read {len((input_lines))} lines")
 
 
-### read functions
+# %% read functions
 def parse_all_lines(lines: list[str], func):
     out = [func(line) for line in lines]
     return out
 
 
-### util defenitions
+# %% util defenitions
 
 
-### parse input
+# %% parse input
 def parse_line(line: str):
     # TODO
     return line
@@ -64,30 +48,31 @@ with timed():
     teststart = parse_all_lines(test_lines, parse_line)
     start = parse_all_lines(input_lines, parse_line)
 
-### main
+# %% main
 
 
-###
-# PART 2
-###
+# %%
+# %% PART 2
+# %%
 p = Path("test2.txt")
 if p.exists():
     test_txt = p.read_text()
     test_lines = test_txt.splitlines()
 
 
-### util defenitions
+# %% util defenitions
 
 
-### parse input - cange parse_line if necessary
+# %%
+# parse input - change parse_line if necessary
 # change parse_line if necessary
 def parse_line_2(line: str):
     # TODO
-    return line
+    return parse_line(line)
 
 
 with timed():
     teststart = parse_all_lines(test_lines, parse_line_2)
     start = parse_all_lines(input_lines, parse_line_2)
 
-### main
+# %% main
