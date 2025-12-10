a934cc8978ca13ff0436de4a546bb8fabd5e2127
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-12-10 10:54:32 +0100

    graph utils: bfs

--- graph_utils.py
+++ graph_utils.py
@@ -1,4 +1,7 @@
-from typing import Any
+from collections import deque
+from typing import Any, Callable, Iterable
+
+deque
 
 Graph = dict[Any, dict[Any, float]]
 
@@ -11,3 +14,19 @@ def iden_cross(d) -> Graph:
             G[i][j] = 1 if j in d[i] else float("inf")
         G[i][i] = 0
     return G
+
+
+def bfs(init: Any, get_children: Callable[[Any], Iterable[Any]], goal: Any):
+    """Breath First Search"""
+    if init == goal:
+        return ()
+
+    queue: deque[tuple[Any, ...]] = deque([(init,)])
+    while True:
+        hist = queue.popleft()
+        for child in get_children(hist[-1]):
+            curr = hist + (child,)
+            if child == goal:
+                return curr
+
+            queue.append(curr)
